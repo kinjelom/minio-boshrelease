e48e83f37743203101a162dbe9d864f7795d275a
Mariusz Olejnik <mariusz.olejnik@infinite.pl>
2023-04-06 08:52:46 +0200

    version 2023-03-24T21-41-23Z

---
+++ add-blobs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+source ./src/blobs-versions.env
+source ./rel.env
+
+mkdir -p $TMP_DIR
+
+function down_add_blob {
+  FILE=$1
+  URL=$2
+  if [ ! -f blobs/${FILE} ];then
+    echo "Downloads resource from the Internet ($URL -> $TMP_DIR/$FILE)"
+    curl -L $URL --output $TMP_DIR/$FILE
+    echo "Adds blob ($TMP_DIR/$FILE -> $FILE), starts tracking blob in config/blobs.yml for inclusion in packages"
+    bosh add-blob $TMP_DIR/$FILE $FILE
+  fi
+}
+
+down_add_blob "minio-${MINIO_VERSION}" "https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${MINIO_VERSION}"
+down_add_blob "mc-${MC_VERSION}" "https://dl.minio.io/client/mc/release/linux-amd64/archive/mc.RELEASE.${MC_VERSION}"
+
+
+echo "Download blobs into blobs/ based on config/blobs.yml"
+bosh sync-blobs
+
+echo "Upload previously added blobs that were not yet uploaded to the blobstore. Updates config/blobs.yml with returned blobstore IDs."
+bosh upload-blobs
